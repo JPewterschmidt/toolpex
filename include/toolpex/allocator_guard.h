a67b2f8079c11e8fcacd989e5f569a16d2d5885c
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-01-06 16:45:14 +0800

    update allocated_ptr

--- include/toolpex/allocator_guard.h
+++ include/toolpex/allocator_guard.h
@@ -9,6 +9,9 @@
 
 TOOLPEX_NAMESPACE_BEG
 
+/*! \brief A RAII wrapper for allocated space by any allocator similar to `::std::unique_ptr`.
+ *  \tparam Alloc The allocator type.
+ */
 template<typename Alloc>
 class allocator_ptr : public toolpex::move_only
 {
@@ -31,15 +34,43 @@ public:
     {
     }
 
+    allocator_ptr& operator=(allocator_ptr&& other) noexcept
+    {
+        deallocate();
+        m_alloc = ::std::exchange(other.m_alloc);
+        m_p = ::std::exchange(other.m_p);
+        return *this;
+    }
+
+    /*! \brief Give up the ownership of guarded space, but won't deallocate the space.
+     *  \see `give_up_ownership()`
+     */
     allocator_ptr& operator=(::std::nullptr_t) noexcept
+    {
+        give_up_ownership();
+    }
+
+    /*! \brief Give up the ownership of guarded space, but won't deallocate the space
+     *  
+     *  When a `allocator_ptr` object hold the ownership of a range of space, 
+     *  it will be deallocate when the object destructs.
+     */
+    void give_up_ownership() noexcept
     {
         m_p = nullptr;
     }
 
     operator pointer() noexcept { return m_p; }
     pointer operator ->() noexcept { return m_p; }
+    operator bool() const noexcept { return m_p != nullptr; }
 
     ~allocator_ptr() noexcept
+    {
+        deallocate();
+    }
+
+private:
+    void deallocate() noexcept
     {
         if (m_p && m_alloc)
         {
@@ -53,11 +84,17 @@ private:
     pointer m_p{};
 };
 
+/*! \brief  
+ *  \param  alloc   The reference of a allocator.
+ *  \param  size    The numebr of object(uninitialized) of `::std::allocator_traits<Alloc>::value_type`.
+ *  \return A RAII wrapper just like unique_ptr, but for allocated space from a allocator.
+ *          This wrapper contains sizeof(::std::allocator_traits<Alloc>::value_type) bytes of space.
+ */
 template<typename Alloc>
-auto allocate_guarded(Alloc& alloc)
+auto allocate_guarded(Alloc& alloc, size_t number_of_obj = 1)
 {
     return allocator_ptr{ 
-        alloc, ::std::allocator_traits<Alloc>::allocate(alloc, 1) 
+        alloc, ::std::allocator_traits<Alloc>::allocate(alloc, number_of_obj) 
     };
 }
 
