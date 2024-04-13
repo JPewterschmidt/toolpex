3b9ed4ee75e9a75efc5c95a0af69a43818c36807
Jeremy Pewterschmidt <jeremypewterschmidt@gmail.com>
2024-04-13 17:50:37 +0800

    make the unittests of skiplist more formally

--- include/toolpex/skip_list.h
+++ include/toolpex/skip_list.h
@@ -409,7 +409,7 @@ public:
             insert(::std::forward<decltype(item)>(item));
     }
 
-    void erase(const key_type& key)
+    void erase(const key_type& key, pointer out_val = nullptr)
     {
         ::std::vector<node*> update(max_level());
         node* x = next(left_nearest(key, update));
@@ -419,6 +419,10 @@ public:
             {
                 if ((*update[i])[i] != x) break;
                 (*update[i])[i] = (*x)[i];
+            }
+            if (out_val)
+            {
+                *out_val = ::std::move(x->value());
             }
             demake_node(x);
             while (level() >= 1 && (*head_node_ptr())[level()-1] == end_node_ptr())
