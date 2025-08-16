f1f0816d79e1274835e7983631bcd538dd45fc76
Jeremy Pewterschmidt (Shixin Wang) <jeremypewterschmidt@gmail.com>
2025-08-17 02:23:39 +0800

    Fix buggy usage of perfect forwarding

--- include/toolpex/functional.h
+++ include/toolpex/functional.h
@@ -19,7 +19,7 @@ TOOLPEX_NAMESPACE_BEG
 
 struct do_nothing
 {
-    constexpr void operator()([[maybe_unused]] auto&& args...) const noexcept { }
+    constexpr void operator()([[maybe_unused]] auto&&... args) const noexcept { }
     constexpr void operator()() const noexcept { }
 };
 
